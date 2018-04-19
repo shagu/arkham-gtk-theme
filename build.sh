e09df9f6308fc45a86a64effa3d02bfdfb93b485
shagu <ericmauser@googlemail.com>
2018-04-19 23:03:11 +0200

    build: rewrite

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-OUTPUT="~/.themes/Adwaita-Atom"
 
 function exchange() {
   gresource extract /usr/lib/libgtk-3.so /org/gtk/libgtk/theme/Adwaita/gtk-contained-dark.css \
-    | grep -i "$1" >> $OUTPUT/gtk-3.0/gtk.css
-  find $OUTPUT -type f -exec sed -i "s/$1/$2/gI" {} \;
+    | grep -i "$1" >> ./gtk-3.0/gtk.css
+
+  find gtk-* -type f -exec sed -i "s/$1/$2/gI" {} \;
 }
 
-rm -rf $OUTPUT
-cp -rf /usr/share/themes/Adwaita-dark $OUTPUT
+# clean all gtk data
+rm -rf gtk-*
 
-exchange 33393B 1D1F21
-exchange 232729 2B2E31
+# copy gtk2 theme
+cp -rf /usr/share/themes/Adwaita-dark/gtk-2.0 .
 
-echo 'menubar, .menubar { background: #161719; }' >> $OUTPUT/gtk-3.0/gtk.css
+# create gtk3 theme
+mkdir gtk-3.0
+echo '@import url("resource:///org/gtk/libgtk/theme/Adwaita/gtk-contained-dark.css");' > gtk-3.0/gtk.css
+
+exchange "33393B" "1D1F21" # base
+exchange "232729" "2B2E31" # textfields
+exchange "272C2D" "1D1F21" # pluma, tab background
+exchange "262b2d" "161719" # menu entry
+
+exchange "215d9c" "4a89c7" # accent color
+exchange "184472" "4a89c7" # selection outline
+exchange "4a90d9" "ffffff" # menutext highlight
+
+echo 'menubar, .menubar { background: #161719; }' >> ./gtk-3.0/gtk.css
